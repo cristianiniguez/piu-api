490f87e248aed18d5162b80ed7dce1b2ac8c95fe
cristianiniguez <felizpc1@gmail.com>
2024-05-14 16:10:24 +0000

    added naissance

--- src/data/songs/index.ts
+++ src/data/songs/index.ts
@@ -1,9 +1,11 @@
 import the1stDanceFloor from './the-1st-dance-floor'
 import secondUltimateRemix from './2nd-ultimate-remix'
+import thirdObg from './3rd-obg'
 
 const mapEditionToVersionMap = {
   'the-1st-dance-floor': the1stDanceFloor,
-  '2nd-ultimate-remix': secondUltimateRemix
+  '2nd-ultimate-remix': secondUltimateRemix,
+  '3rd-obg': thirdObg
 }
 
 export default mapEditionToVersionMap
