345bf1c65a15f2c359f820e06a0d134162ad5ff6
cristianiniguez <felizpc1@gmail.com>
2024-05-15 18:47:31 +0000

    added all i want for x-mas

--- src/data/songs/index.ts
+++ src/data/songs/index.ts
@@ -2,12 +2,14 @@ import the1stDanceFloor from './the-1st-dance-floor'
 import secondUltimateRemix from './2nd-ultimate-remix'
 import thirdObg from './3rd-obg'
 import theObgSeasonEvolution from './the-obg-season-evolution'
+import thePerfectCollection from './the-perfect-collection'
 
 const mapEditionToVersionMap = {
   'the-1st-dance-floor': the1stDanceFloor,
   '2nd-ultimate-remix': secondUltimateRemix,
   '3rd-obg': thirdObg,
-  'the-obg-season-evolution': theObgSeasonEvolution
+  'the-obg-season-evolution': theObgSeasonEvolution,
+  'the-perfect-collection': thePerfectCollection
 }
 
 export default mapEditionToVersionMap
