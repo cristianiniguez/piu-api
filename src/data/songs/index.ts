3f0062d45a471348fc69e71e59606baba5de90be
cristianiniguez <felizpc1@gmail.com>
2024-05-16 15:25:30 +0000

    added chicken wing

--- src/data/songs/index.ts
+++ src/data/songs/index.ts
@@ -3,13 +3,15 @@ import secondUltimateRemix from './2nd-ultimate-remix'
 import thirdObg from './3rd-obg'
 import theObgSeasonEvolution from './the-obg-season-evolution'
 import thePerfectCollection from './the-perfect-collection'
+import extra from './extra'
 
 const mapEditionToVersionMap = {
   'the-1st-dance-floor': the1stDanceFloor,
   '2nd-ultimate-remix': secondUltimateRemix,
   '3rd-obg': thirdObg,
   'the-obg-season-evolution': theObgSeasonEvolution,
-  'the-perfect-collection': thePerfectCollection
+  'the-perfect-collection': thePerfectCollection,
+  'extra': extra
 }
 
 export default mapEditionToVersionMap
