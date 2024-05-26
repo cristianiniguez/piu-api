2a8e91400ae00779564ae591c5b165d8b4871c9c
cristianiniguez <felizpc1@gmail.com>
2024-05-26 02:00:10 +0000

    added radetzky can can

--- src/data/songs/extra/default.ts
+++ src/data/songs/extra/default.ts
@@ -1,3 +1,11 @@
+const premiereToPremiere3Pattern: StepHistoryPattern = level => ({
+  'the-premiere::default': 0,
+  'the-prex::default': level,
+  'the-rebirth::default': 0,
+  'the-prex-2::default': level,
+  'the-premiere-3::default': 0
+})
+
 const songsMap: Record<string, Song> = {
   'chicken-wing': {
     id: 'chicken-wing',
@@ -83,56 +91,46 @@ const songsMap: Record<string, Song> = {
     bpm: 189,
     type: 'arcade',
     category: 'original',
-    steps: (() => {
-      const premiereToPremiere3Pattern: StepHistoryPattern = level => ({
-        'the-premiere::default': 0,
-        'the-prex::default': level,
-        'the-rebirth::default': 0,
-        'the-prex-2::default': level,
-        'the-premiere-3::default': 0
-      })
-
-      return {
-        single: [
-          { 'fiesta-ex::v1.50': 4 },
-          { 'fiesta-ex::v1.50': 7, 'phoenix::v1.00.0': 8 },
-          {
-            'extra::default': 9,
-            ...premiereToPremiere3Pattern(9),
-            'the-prex-3::default': 9,
-            'exceed::default': 6,
-            'exceed-2::default': 7,
-            'nx::default': 8,
-            'phoenix::v1.00.0': 9
-          },
-          { 'nx::default': 9, 'nx2::default': 10, 'nxa::default': 13 },
-          {
-            'extra::default': 4,
-            ...premiereToPremiere3Pattern(9),
-            'the-prex-3::default': 7,
-            'exceed::default': 14,
-            'exceed-2::default': 15,
-            'xx::v1.00.1': 16,
-            'phoenix::v1.00.0': 17
-          },
-          { 'nx2::default': 19, 'xx::v1.00.1': 20, 'phoenix::v1.00.0': 21 }
-        ],
-        double: [
-          {
-            'extra::default': 4,
-            ...premiereToPremiere3Pattern(10),
-            'the-prex-3::default': 10,
-            'exceed::default': 7,
-            'exceed-2::default': 8,
-            'nx::default': 11,
-            'xx::v1.00.1': 13,
-            'phoenix::v1.00.0': 15
-          },
-          { 'exceed::default': 19, 'exceed-2::default': 21, 'xx::v1.00.1': 22 },
-          { 'nx::default': 22, 'nxa::default': 23 }
-        ]
-      }
-    })()
+    steps: {
+      single: [
+        { 'fiesta-ex::v1.50': 4 },
+        { 'fiesta-ex::v1.50': 7, 'phoenix::v1.00.0': 8 },
+        {
+          'extra::default': 9,
+          ...premiereToPremiere3Pattern(9),
+          'the-prex-3::default': 9,
+          'exceed::default': 6,
+          'exceed-2::default': 7,
+          'nx::default': 8,
+          'phoenix::v1.00.0': 9
+        },
+        { 'nx::default': 9, 'nx2::default': 10, 'nxa::default': 13 },
+        {
+          'extra::default': 4,
+          ...premiereToPremiere3Pattern(9),
+          'the-prex-3::default': 7,
+          'exceed::default': 14,
+          'exceed-2::default': 15,
+          'xx::v1.00.1': 16,
+          'phoenix::v1.00.0': 17
+        },
+        { 'nx2::default': 19, 'xx::v1.00.1': 20, 'phoenix::v1.00.0': 21 }
+      ],
+      double: [
+        {
+          'extra::default': 4,
+          ...premiereToPremiere3Pattern(10),
+          'the-prex-3::default': 10,
+          'exceed::default': 7,
+          'exceed-2::default': 8,
+          'nx::default': 11,
+          'xx::v1.00.1': 13,
+          'phoenix::v1.00.0': 15
+        },
+        { 'exceed::default': 19, 'exceed-2::default': 21, 'xx::v1.00.1': 22 },
+        { 'nx::default': 22, 'nxa::default': 23 }
+      ]
+    }
   },
   'mistake': {
     id: 'mistake',
@@ -179,6 +177,42 @@ const songsMap: Record<string, Song> = {
         'double-p': [{ 'fiesta::v1.01': 6, 'prime-2::v1.00.0': 0 }]
       }
     })()
+  },
+  'radetzky-can-can': {
+    id: 'radetzky-can-can',
+    name: 'Radetzky Can Can',
+    artist: 'BanYa',
+    bpm: 197,
+    type: 'arcade',
+    category: 'original',
+    steps: {
+      single: [
+        { 'xx::v2.05.0': 8, 'phoenix::v1.00.0': 7 },
+        { 'xx::v2.05.0': 13, 'phoenix::v1.00.0': 12 },
+        {
+          'extra::default': 6,
+          ...premiereToPremiere3Pattern(10),
+          'the-prex-3::default': 8,
+          'exceed::default': 12,
+          'zero::default': 0,
+          'xx::v1.00.1': 16
+        },
+        { 'xx::v1.00.1': 20 }
+      ],
+      double: [
+        {
+          'extra::default': 6,
+          ...premiereToPremiere3Pattern(10),
+          'the-prex-3::default': 7,
+          'exceed::default': 15,
+          'exceed-2::default': 14,
+          'zero::default': 0,
+          'xx::v1.00.1': 17,
+          'phoenix::v1.00.0': 18
+        },
+        { 'xx::v1.00.1': 22 }
+      ]
+    }
   }
 }
 
