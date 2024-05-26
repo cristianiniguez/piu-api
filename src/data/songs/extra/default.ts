9536e61060077fa4ebe7e734ee9d91caa5930101
cristianiniguez <felizpc1@gmail.com>
2024-05-26 01:44:25 +0000

    added final audition ep 1

--- src/data/songs/extra/default.ts
+++ src/data/songs/extra/default.ts
@@ -75,6 +75,64 @@ const songsMap: Record<string, Song> = {
         }
       ]
     }
+  },
+  'final-audition-ep-1': {
+    id: 'final-audition-ep-1',
+    name: 'Final Audition Ep. 1',
+    artist: 'BanYa',
+    bpm: 189,
+    type: 'arcade',
+    category: 'original',
+    steps: (() => {
+      const premiereToPremiere3Pattern: StepHistoryPattern = level => ({
+        'the-premiere::default': 0,
+        'the-prex::default': level,
+        'the-rebirth::default': 0,
+        'the-prex-2::default': level,
+        'the-premiere-3::default': 0
+      })
+
+      return {
+        single: [
+          { 'fiesta-ex::v1.50': 4 },
+          { 'fiesta-ex::v1.50': 7, 'phoenix::v1.00.0': 8 },
+          {
+            'extra::default': 9,
+            ...premiereToPremiere3Pattern(9),
+            'the-prex-3::default': 9,
+            'exceed::default': 6,
+            'exceed-2::default': 7,
+            'nx::default': 8,
+            'phoenix::v1.00.0': 9
+          },
+          { 'nx::default': 9, 'nx2::default': 10, 'nxa::default': 13 },
+          {
+            'extra::default': 4,
+            ...premiereToPremiere3Pattern(9),
+            'the-prex-3::default': 7,
+            'exceed::default': 14,
+            'exceed-2::default': 15,
+            'xx::v1.00.1': 16,
+            'phoenix::v1.00.0': 17
+          },
+          { 'nx2::default': 19, 'xx::v1.00.1': 20, 'phoenix::v1.00.0': 21 }
+        ],
+        double: [
+          {
+            'extra::default': 4,
+            ...premiereToPremiere3Pattern(10),
+            'the-prex-3::default': 10,
+            'exceed::default': 7,
+            'exceed-2::default': 8,
+            'nx::default': 11,
+            'xx::v1.00.1': 13,
+            'phoenix::v1.00.0': 15
+          },
+          { 'exceed::default': 19, 'exceed-2::default': 21, 'xx::v1.00.1': 22 },
+          { 'nx::default': 22, 'nxa::default': 23 }
+        ]
+      }
+    })()
   }
 }
 
