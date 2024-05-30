6e5b15a3cf385073506789781b99f9c63ad5921c
cristianiniguez <felizpc1@gmail.com>
2024-05-30 16:04:46 +0000

    installed chakra

---
+++ apps/ui/app/providers.tsx
@@ -0,0 +1,10 @@
+'use client'
+
+import { FC, PropsWithChildren } from 'react'
+import { ChakraProvider } from '@chakra-ui/react'
+
+const Providers: FC<PropsWithChildren> = ({ children }) => (
+  <ChakraProvider>{children}</ChakraProvider>
+)
+
+export default Providers
