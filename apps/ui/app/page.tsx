c17bd666d915ff65dbe91f53da7507065ed9ffcd
cristianiniguez <felizpc1@gmail.com>
2024-06-08 04:05:23 +0000

    redirecting from home to randomizer

--- apps/ui/app/page.tsx
+++ apps/ui/app/page.tsx
@@ -1,3 +1,9 @@
-export default function Home() {
-  return <main>Hello</main>
+import { redirect } from 'next/navigation'
+import API from '@/api'
+
+export default async function Home() {
+  const editions = await API.getEditions()
+  const versionIds = Object.values(editions).flatMap(({ versionIds }) => versionIds)
+  const lastVersionId = versionIds[versionIds.length - 1] ?? 'NOT_A_VERSION'
+  redirect(`/randomizer/${lastVersionId}/form`)
 }
--- apps/ui/app/randomizer/layout.tsx
+++ apps/ui/app/randomizer/layout.tsx
@@ -1,4 +1,4 @@
-import { PropsWithChildren, ReactNode } from 'react'
+import { PropsWithChildren } from 'react'
 import { Box, Grid } from '@chakra-ui/react'
 import API from '@/api'
 import Sidebar from '@/components/randomizer/Sidebar/Sidebar'
