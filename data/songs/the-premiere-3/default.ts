72f496cd93f01cc276aeaf9707cde44b5580d6b0
cristianiniguez <felizpc1@gmail.com>
2024-06-09 17:38:58 +0000

    added bee

--- data/songs/the-premiere-3/default.ts
+++ data/songs/the-premiere-3/default.ts
@@ -67,6 +67,60 @@ const songsMap: Record<string, SongWithStepsHistories> = {
         { 'prime-2::v2.05.0': 23, 'phoenix::v1.00.0': 24 }
       ]
     }
+  },
+  'bee': {
+    id: 'bee',
+    name: 'Bee',
+    artist: 'BanYa',
+    bpm: 160,
+    type: 'arcade',
+    category: 'original',
+    steps: {
+      single: [
+        {
+          'the-premiere-3::default': 5,
+          'exceed::default': 3,
+          'nxa::default': 4,
+          'phoenix::v1.00.0': 0
+        },
+        { 'fiesta-ex::v1.50': 7 },
+        { 'fiesta-ex::v1.50': 10 },
+        {
+          'the-premiere-3::default': 8,
+          'exceed::default': 6,
+          'zero::default': 9,
+          'nx::default': 8,
+          'nxa::default': 11,
+          'xx::v1.00.1': 12
+        },
+        {
+          'the-premiere-3::default': 9,
+          'exceed::default': 12,
+          'exceed-2::default': 13,
+          'nx::default': 15,
+          'xx::v1.00.1': 16,
+          'phoenix::v1.00.0': 17
+        },
+        { 'nx::default': 18, 'nxa::default': 19 }
+      ],
+      double: [
+        {
+          'the-premiere-3::default': 9,
+          'nx::default': 11,
+          'xx::v1.00.1': 12,
+          'phoenix::v1.00.0': 15
+        },
+        { 'fiesta-ex::v1.20': 19, 'xx::v1.00.1': 20 },
+        {
+          'exceed::default': 18,
+          'exceed-2::default': 20,
+          'nx::default': 21,
+          'xx::v1.00.1': 22,
+          'phoenix::v1.00.0': 23
+        },
+        { 'nx::default': 23, 'xx::v1.00.1': 24 }
+      ]
+    }
   }
 }
 
